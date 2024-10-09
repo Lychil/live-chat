b1686a32e5f04354092590489b9b389b052e84b2
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2024-10-10 01:02:34 +0300

    fix

--- server/controllers/chats.controller.js
+++ server/controllers/chats.controller.js
@@ -6,7 +6,6 @@ class ChatsController {
         const { id } = req.body;
         const cookies = req.cookies;
         const chat = await db.query('INSERT INTO chats (buddy, user_id) VALUES ($1, $2) RETURNING *', [id, cookies.auth.id]);
-        console.log('create')
         res.json(chat.rows);
     }
 }
